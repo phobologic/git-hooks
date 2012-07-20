e7aa8b96eefd7a2fde141cd3698a76e0a82ddfa2
Michael Barrett <loki77@gmail.com>
2012-07-20 14:34:59 -0700

    Comments

--- pre-commit/check-flake8.sh
+++ pre-commit/check-flake8.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+#
 # Auto-check for pep8 so I don't check in bad code
+#
 
 #FILES=$(git diff --cached --name-status | grep -v ^D | awk '$1 $2 { print $2}' | grep -e .py$)
 FILES=$(git diff --cached --name-status | cut -f2- | grep -e '.py$')
